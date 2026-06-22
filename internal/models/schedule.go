e9dfcf24e58f5264fc00dd11d0aef650abfc4452
aeon022 <gerwin.weiher@gmail.com>
2026-06-22 19:23:18 +0200

    Initialize Astro landing page for postctl with CSS terminal, TUI interactive simulator, and Polar.sh buttons

--- internal/models/schedule.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "time"
-
-// ScheduledPost repräsentiert einen geplanten Post für den Scheduler
-type ScheduledPost struct {
-	PostID      string    `json:"post_id"`
-	ScheduledAt time.Time `json:"scheduled_at"`
-}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/aeon022/postctl/cmd"
-
-func main() {
-	cmd.Execute()
-}
