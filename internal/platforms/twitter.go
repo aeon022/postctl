0032123955a55214022604037bf8a9dc5241446c
aeon022 <gerwin.weiher@gmail.com>
2026-06-27 09:54:51 +0200

    fix: update Twitter GraphQL queryId and feature flags to SiM_cAu83R0wnrpmKQQSEw

--- internal/platforms/twitter.go
+++ internal/platforms/twitter.go
@@ -554,13 +554,8 @@ func (t *TwitterPlatform) postCookieBased(ctx context.Context, post *models.Post
 		payload := map[string]interface{}{
 			"variables": vars,
 			"features": map[string]interface{}{
-				"rweb_tipjar_consumption_enabled":                                         true,
-				"responsive_web_graphql_exclude_directive_enabled":                        true,
-				"verified_phone_label_enabled":                                            false,
 				"creator_subscriptions_tweet_preview_api_enabled":                         true,
-				"responsive_web_graphql_timeline_navigation_enabled":                      true,
-				"responsive_web_graphql_skip_user_profile_image_extensions_enabled":       false,
-				"communities_web_enable_dependency_on_graphql_timeline_label":             true,
+				"c9s_tweet_anatomy_moderator_badge_enabled":                               true,
 				"tweetypie_unmention_optimization_enabled":                                true,
 				"responsive_web_edit_tweet_api_enabled":                                   true,
 				"graphql_is_translatable_rweb_tweet_is_translatable_enabled":              true,
@@ -568,19 +563,19 @@ func (t *TwitterPlatform) postCookieBased(ctx context.Context, post *models.Post
 				"longform_notetweets_consumption_enabled":                                 true,
 				"responsive_web_twitter_article_tweet_consumption_enabled":                true,
 				"tweet_awards_web_tipping_enabled":                                        false,
-				"creator_subscriptions_quote_tweet_preview_api_enabled":                   false,
+				"longform_notetweets_rich_text_read_enabled":                              true,
+				"longform_notetweets_inline_media_enabled":                                true,
+				"rweb_video_timestamps_enabled":                                           true,
+				"responsive_web_graphql_exclude_directive_enabled":                        true,
+				"verified_phone_label_enabled":                                            false,
 				"freedom_of_speech_not_reach_fetch_enabled":                               true,
 				"standardized_nudges_misinfo":                                             true,
 				"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": true,
-				"rweb_video_timestamps_enabled":                                           true,
-				"longform_notetweets_rich_text_read_enabled":                              true,
-				"longform_notetweets_inline_expand_super_expanded_timeline_read_enabled":  true,
-				"responsive_web_enhance_cards_enabled":                                    false,
 			},
 			"fieldToggles": map[string]interface{}{
 				"withArticleRelationships": false,
 			},
-			"queryId": "oB30Zs7a1q7sbIWD0g756A",
+			"queryId": "SiM_cAu83R0wnrpmKQQSEw",
 		}
 
 		bodyBytes, err := json.Marshal(payload)
@@ -588,7 +583,7 @@ func (t *TwitterPlatform) postCookieBased(ctx context.Context, post *models.Post
 			return "", err
 		}
 
-		reqURL := "https://twitter.com/i/api/graphql/oB30Zs7a1q7sbIWD0g756A/CreateTweet"
+		reqURL := "https://twitter.com/i/api/graphql/SiM_cAu83R0wnrpmKQQSEw/CreateTweet"
 		req, err := http.NewRequestWithContext(ctx, "POST", reqURL, bytes.NewReader(bodyBytes))
 		if err != nil {
 			return "", err
